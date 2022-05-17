0a3f967f0d54b7ad6b15af50c3dd24cdfc299706
azg <abzzgd@gmail.com>
2022-05-17 11:04:24 +0300

    database insert

--- a1.pl
+++ a1.pl
@@ -4,13 +4,20 @@ use strict;
 use warnings;
 
 use LWP::Simple;
+use utf8;
 binmode(STDOUT,':utf8');
 
+use DBI;
+
+my $db = DBI->connect("dbi:SQLite:dbname=bashorg.db","","");
 my $page = get("http://bashorg.org/");
 
 for my $el ($page =~ /class="q".+?(#\d+.*?)<\/span/sg) {
-  print $el,"\n\n";
   $el =~ /#(\d*).*?\|\s(.*)\s\|.*?class="quote">(.*)<\/div>.*>(\d*)/s;
   
-  print $1,"\n",$2,"\n",$3,"\n",$4,"\n\n\n";
+  $db->do("
+    INSERT INTO quotes (number,date,content,rating)
+    VALUES (?,?,?,?)",
+    undef,$1,$2,$3,$4
+  );
 }
